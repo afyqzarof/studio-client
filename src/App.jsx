5efe15d4de3bfe1b5ed89e2c6c7220fee47696be
afyqeyzar <norafyqe@gmail.com>
2024-01-23 14:59:09 +0000

    feature: add and style splash page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import BoardPage from "./pages/BoardPage/BoardPage";
-import TestingPage from "./pages/TestingPage/TestingPage";
-import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
+// import LoginPage from "./pages/LoginPage/LoginPage";
+// import BoardPage from "./pages/BoardPage/BoardPage";
+// import TestingPage from "./pages/TestingPage/TestingPage";
+// import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/login" element={<LoginPage />} />
         <Route path="/board/:boardId" element={<BoardPage />} />
         <Route path="/dashboard" element={<DashBoardPage />} />
-        <Route path="/test" element={<TestingPage />} />
+        <Route path="/test" element={<TestingPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
