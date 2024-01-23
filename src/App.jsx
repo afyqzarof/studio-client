a9391098fb7ddf2d352f51b37375901ee4ffdd6b
afyqeyzar <norafyqe@gmail.com>
2024-01-23 14:55:47 +0000

    chore: Initial page and folder setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import BoardPage from "./pages/BoardPage/BoardPage";
+import TestingPage from "./pages/TestingPage/TestingPage";
+import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
 
 function App() {
-  return <h1>studio</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/board/:boardId" element={<BoardPage />} />
+        <Route path="/dashboard" element={<DashBoardPage />} />
+        <Route path="/test" element={<TestingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
