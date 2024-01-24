5585b94669458949425791be3703d7ba48253ffb
afyqeyzar <norafyqe@gmail.com>
2024-01-24 21:36:44 +0000

    feature: better ux for file upload

--- src/components/ModalUpload/ModalUpload.jsx
+++ src/components/ModalUpload/ModalUpload.jsx
@@ -2,27 +2,50 @@ import "./ModalUpload.scss";
 import Popup from "reactjs-popup";
 import axios from "axios";
 import { useDropzone } from "react-dropzone";
+import { useState, useCallback } from "react";
 
 const ModalUpload = () => {
   const baseUrl = import.meta.env.VITE_BASE_URL;
+  const [myFiles, setMyFiles] = useState(null);
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      // setMyFiles([...acceptedFiles]);
+      setMyFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+    [myFiles]
+  );
 
   const handleCreateUploadPin = async (e) => {
     e.preventDefault();
-    const file = acceptedFiles[0];
+
+    const file = myFiles[0];
+    console.log(file);
 
     const formData = new FormData();
     formData.append("file", file);
     const { data } = await axios.post(baseUrl + "/api/upload", formData);
     console.log(data);
+    setMyFiles(null);
   };
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
     accept: {
       "image/jpeg": [".jpg", ".jpeg"],
       "image/png": [".png"],
     },
     maxSize: 500000,
+    onDrop,
   });
+  const handleCancel = () => {
+    setMyFiles(null);
+  };
   return (
     <Popup
       trigger={<button className="btn-container__btn">upload</button>}
@@ -30,7 +53,9 @@ const ModalUpload = () => {
       nested>
       {(close) => (
         <article className="modal">
-          <h2 className="modal__title">Upload a file</h2>
+          <h2 className="modal__title">
+            {!myFiles ? "upload a file" : "file preview"}
+          </h2>
           <form
             encType="multipart/form-data"
             className="file-form"
@@ -38,18 +63,27 @@ const ModalUpload = () => {
               handleCreateUploadPin(e);
               close();
             }}>
-            <div {...getRootProps({ className: "dropzone" })}>
-              <input {...getInputProps()} name="file" />
-              <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
+            {!myFiles ? (
+              <div {...getRootProps({ className: "dropzone" })}>
+                <input {...getInputProps()} name="file" />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            ) : (
+              <img src={myFiles[0].preview} className="file-form__preview" />
+            )}
 
-            <p>{acceptedFiles.length === 0 ? "" : acceptedFiles[0].path}</p>
-            {acceptedFiles.length === 0 || (
+            <p className="file-form__name">{myFiles && myFiles[0].path}</p>
+            {myFiles && (
               <button type="submit" className="file-form__btn">
                 create pin
               </button>
             )}
           </form>
+          {myFiles && (
+            <button onClick={handleCancel} className="modal__cancel">
+              Cancel
+            </button>
+          )}
         </article>
       )}
     </Popup>
