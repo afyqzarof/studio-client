1c8ea0c663e643107dc33e3731925fcbc66a5220
afyqeyzar <norafyqe@gmail.com>
2024-01-28 13:55:34 +0000

    refactor: style modal box

--- src/components/ModalUpload/ModalUpload.jsx
+++ src/components/ModalUpload/ModalUpload.jsx
@@ -78,9 +78,6 @@ const ModalUpload = () => {
         onRequestClose={closeModal}
         contentLabel="Example Modal">
         <article className="upload">
-          <h2 className="upload__title">
-            {!myFiles ? "upload a file" : "file preview"}
-          </h2>
           <form
             encType="multipart/form-data"
             className="file-form"
@@ -88,36 +85,46 @@ const ModalUpload = () => {
               handleCreateUploadPin(e);
               close();
             }}>
+            {/* <h2 className="upload__title">
+              {!myFiles ? "upload a file" : "file preview"}
+            </h2> */}
             {!myFiles ? (
               <>
+                <div className="file-form__left">
+                  <h2 className="upload__title">upload a file</h2>
+                  <p className="file-form__info">
+                    please make sure your files are .jgp, .jpeg <br /> or .png
+                    and don't exceed 500KB
+                  </p>
+                </div>
                 <div {...getRootProps({ className: "dropzone" })}>
                   <input {...getInputProps()} name="file" />
                   <p className="upload__dnd">
-                    drag and drop some files here, or click to select files
+                    drag and drop some files here,
+                    <br /> or click to select files
                   </p>
                 </div>
-
-                <p className="file-form__info">
-                  please make sure your files are .jgp, .jpeg or .png and don't
-                  exceed 500KB
-                </p>
               </>
             ) : (
               <>
+                <div className="file-form__info-left">
+                  <div>
+                    <h2 className="upload__title">file preview</h2>
+                    <p className="file-form__name">{myFiles[0].path}</p>
+                  </div>
+                  <div>
+                    <button type="submit" className="file-form__btn">
+                      create pin
+                    </button>
+                    <p onClick={handleCancel} className="upload__cancel">
+                      cancel
+                    </p>
+                  </div>
+                </div>
                 <img src={myFiles[0].preview} className="file-form__preview" />
-                <p className="file-form__name">{myFiles[0].path}</p>
-
-                <button type="submit" className="file-form__btn">
-                  create pin
-                </button>
               </>
             )}
           </form>
-          {myFiles && (
-            <button onClick={handleCancel} className="upload__cancel">
-              cancel
-            </button>
-          )}
         </article>
       </Modal>
     </>
