2a1f77b7bb9f0c20e6651c17059744cdd052a12c
afyqeyzar <norafyqe@gmail.com>
2024-01-30 13:44:02 +0000

    feat: add explore board page with functionality

--- src/components/Flow/Flow.jsx
+++ src/components/Flow/Flow.jsx
@@ -1,57 +1,14 @@
-import ReactFlow, { Background, useNodesState } from "reactflow";
+import ReactFlow, { Background } from "reactflow";
 import "reactflow/dist/base.css";
-import YoutubeVidNode from "../YoutubeVidNode/YoutubeVidNode";
-import ColorSelectorNode from "../ColorSelectorNode/ColorSelectorNode";
 import "./Flow.scss";
-import { useRef, useCallback, useState, useEffect } from "react";
+import { useRef, useCallback, useState } from "react";
 import ContextMenu from "../ContextMenu/ContextMenu";
-// import initialNodes from "../../data/initial-nodes.json";
-import TextNode from "../TextNode/TextNode";
 import ToolBar from "../ToolBar/ToolBar";
-import ImageNode from "../ImageNode/ImageNode";
-import SpotifyNode from "../SpotifyNode/SpotifyNode";
-import PinterestNode from "../PinterestNode/PinterestNode";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-
+import useNodeTypes from "../../hooks/useNodeTypes";
+import useFetchPins from "../../hooks/useFetchPins";
+const nodeTypes = useNodeTypes();
 const Flow = ({ isGrid }) => {
-  const nodeTypes = {
-    ColorSelectorNode,
-    YoutubeVidNode,
-    TextNode,
-    ImageNode,
-    SpotifyNode,
-    PinterestNode,
-  };
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const { boardId } = useParams();
-
-  const baseUrl = import.meta.env.VITE_BASE_URL;
-
-  useEffect(() => {
-    const fetchPins = async () => {
-      const { data } = await axios.get(
-        baseUrl + "/api/boards/" + boardId + "/pins"
-      );
-      const formattedPins = data.map((pin) => {
-        return {
-          id: pin.id,
-          type: pin.type,
-          data: JSON.parse(pin.data),
-          position: {
-            x: pin.x_coord,
-            y: pin.y_coord,
-          },
-          style: {
-            height: pin.height,
-            width: pin.height,
-          },
-        };
-      });
-      setNodes(formattedPins);
-    };
-    fetchPins();
-  }, []);
+  const { nodes, onNodesChange } = useFetchPins();
   const [menu, setMenu] = useState(null);
   const ref = useRef(null);
   const onNodeContextMenu = useCallback(
