e552cab5c61ec440c66dce49dda1d127dc40ec10
afyqeyzar <norafyqe@gmail.com>
2024-01-30 10:52:44 +0000

    wip: screen capture does not work

--- src/components/BoardHeader/BoardHeader.jsx
+++ src/components/BoardHeader/BoardHeader.jsx
@@ -3,7 +3,8 @@ import "./BoardHeader.scss";
 import upIconDefault from "../../assets/icons/arrow-N-default.svg";
 import upIconSelected from "../../assets/icons/arrow-N-selected.svg";
 import { useState } from "react";
-import { useReactFlow } from "reactflow";
+import { useReactFlow, getViewportForBounds, getNodesBounds } from "reactflow";
+import { toPng } from "html-to-image";
 
 const BoardHeader = () => {
   const [isIconSelected, setIsIconSelected] = useState(false);
@@ -23,6 +24,40 @@ const BoardHeader = () => {
       };
     });
     console.log(formattedPins);
+  };
+
+  function downloadImage(dataUrl) {
+    const a = document.createElement("a");
+
+    a.setAttribute("download", "reactflow.png");
+    a.setAttribute("href", dataUrl);
+    a.click();
+  }
+  const imageWidth = 1024;
+  const imageHeight = 768;
+  const onClick = () => {
+    // we calculate a transform for the nodes so that all nodes are visible
+    // we then overwrite the transform of the `.react-flow__viewport` element
+    // with the style option of the html-to-image library
+    const nodesBounds = getNodesBounds(getNodes());
+    const transform = getViewportForBounds(
+      nodesBounds,
+      imageWidth,
+      imageHeight,
+      0.5,
+      2
+    );
+
+    toPng(document.querySelector(".react-flow__viewport"), {
+      backgroundColor: "#fff",
+      width: imageWidth,
+      height: imageHeight,
+      style: {
+        width: imageWidth,
+        height: imageHeight,
+        transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+      },
+    }).then(downloadImage);
   };
 
   return (
@@ -46,7 +81,9 @@ const BoardHeader = () => {
         </div>
         <ul className="nav__right-container">
           <button className="nav__btn">collaborate</button>
-          <button className="nav__btn">publish</button>
+          <button className="nav__btn" onClick={onClick}>
+            publish
+          </button>
           <button className="nav__btn" onClick={handleSave}>
             save
           </button>
