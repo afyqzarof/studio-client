b1c7c1c091676b7f7cb056fdeee1ff88a4a7cff4
afyqeyzar <norafyqe@gmail.com>
2024-01-31 16:55:00 +0000

    feat: get user by board

--- src/pages/DashBoardPage/DashBoardPage.jsx
+++ src/pages/DashBoardPage/DashBoardPage.jsx
@@ -2,8 +2,24 @@ import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import "./DashBoardPage.scss";
 import MainHeader from "../../components/MainHeader/MainHeader";
 import boardData from "../../data/boards-data";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const DashBoardPage = () => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const [boards, setBoards] = useState([]);
+  useEffect(() => {
+    const fetchUserBoards = async () => {
+      const token = localStorage.getItem("token");
+      console.log(token);
+
+      const { data } = await axios.get(baseUrl + "/api/users/boards", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setBoards(data);
+    };
+    fetchUserBoards();
+  }, []);
   return (
     <div className="page-wrapper">
       <MainHeader />
@@ -13,15 +29,13 @@ const DashBoardPage = () => {
       <main className="dashboard-main">
         <nav className="dashboard-main__nav">
           <button className="dashboard-main__folder">show all</button>
-          {/* <button className="dashboard-main__folder">project folder</button>
-            <button className="dashboard-main__folder">project folder</button> */}
         </nav>
         <ul className="dashboard-main__projects">
-          {boardData.map((board) => (
+          {boards.map((board) => (
             <li key={board.id}>
               <ProjectCard
                 title={board.title}
-                imgSrc={board.imgSrc}
+                imgSrc={baseUrl + "/api/thumbnails/" + board.thumbnail}
                 description={board.description}
                 date={board.date}
                 category={board.category}
