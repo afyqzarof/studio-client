6b2ae359e18a233f898d899f077a5c48a80cf5a2
afyqeyzar <norafyqe@gmail.com>
2024-02-07 10:58:27 +0000

    wip: delete board

--- src/components/BoardContextMenu/BoardContextMenu.jsx
+++ src/components/BoardContextMenu/BoardContextMenu.jsx
@@ -12,7 +12,6 @@ const BoardContextMenu = ({
       className={`context-menu ${isToggled ? "active" : "not-active"}`}
       ref={contextMenuRef}
       style={{ top: positionY + 2 + "px", left: positionX + 2 + "px" }}>
-      <p className="context-menu__btn">edit</p>
       <p className="context-menu__btn">make public/private</p>
       <p className="context-menu__btn">delete</p>
     </menu>
