6be782c4a408b5a103a64a0c27c28779b2f05bd9
afyqeyzar <norafyqe@gmail.com>
2024-02-28 16:28:40 +0000

    wip: homepage tests

--- src/pages/HomePage/HomePage.test.jsx
+++ src/pages/HomePage/HomePage.test.jsx
@@ -1,9 +1,11 @@
 import { describe, it } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 
 describe("HomePage", () => {
   it("should render", async () => {
-    render(<HomePage />);
+    render(<HomePage />, { wrapper: BrowserRouter });
+    expect(screen.getByText(/articulate/)).toBeInTheDocument();
   });
 });
