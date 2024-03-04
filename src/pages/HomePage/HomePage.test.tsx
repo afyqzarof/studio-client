fa27294da9cea2240729e41ecb4ee8aa139baab3
afyqeyzar <norafyqe@gmail.com>
2024-03-04 15:08:09 +0000

    chore: add home page test

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
